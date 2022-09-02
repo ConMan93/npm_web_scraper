2fc2ddb665b8592bbb76d9e5ded9369bf770c084
monterrozagera <monterrozagerardo@protonmail.com>
2022-09-01 23:58:49 -0700

    add OS compatibility and load yara rules function

--- npm_web_scraper.py
+++ npm_web_scraper.py
@@ -1,19 +1,20 @@
-# Author: Connor Hoffman 001345531
+# Author: Connor Hoffman 001345531 & Gerardo Monterroza
 
-import requests
-import bs4
-import os
-import tarfile
-import yara
 import pymsteams
+import requests
+import tarfile
 import shutil
+import yara
+import bs4
+import sys
+import os
 
 WEBHOOK_URL = 'https://westerngovernorsuniversity.webhook.office.com/webhookb2/8c7994c4-d96f-4b42-a8db-63f8ab84d9c9@cfa792cf-7768-4341-8857-81754c2afa1f/IncomingWebhook/176366163f164c0c87d31cb389e0f6d5/5091c999-0734-4e37-a913-b9dca36decbd'
 
 def get_new_package_names():
     npm_package_names = []
     k = 1
-    while(k < 5):
+    while(k < 2):
         res = requests.get('https://libraries.io/search?order=desc&platforms=npm&sort=created_at&page='+str(k))
         soup = bs4.BeautifulSoup(res.text, 'html.parser')
         i = 4 # Based on the set up of the webpage, this is the first package in the list
@@ -29,13 +30,13 @@ def get_new_package_names():
 
 def download_packages(package_names):
     for package in package_names:
-        os.system('npm pack ' + package + ' --pack-destination .\\npm_packages\\')
+        os.system('npm pack ' + package + ' --pack-destination .' + dir_separator + 'npm_packages' + dir_separator)
 
 def extract_packages():
-    files = os.listdir('C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\npm_packages\\')
+    files = os.listdir(current_working_directory + dir_separator + 'npm_packages' + dir_separator)
     for file in files:
-        ext_file = tarfile.open('C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\npm_packages\\' + file)
-        ext_file.extractall('C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\extracted_packages\\' + file[:-4])
+        ext_file = tarfile.open(current_working_directory + dir_separator + 'npm_packages' + dir_separator + file)
+        ext_file.extractall(current_working_directory + dir_separator + 'extracted_packages' + dir_separator + file[:-4])
         ext_file.close()
 
 def create_card(package_name, filename, yara_matches):
@@ -44,39 +45,62 @@ def create_card(package_name, filename, yara_matches):
     my_teams_message.text(f"The {filename} file in the {package_name} package triggered {yara_matches} yara rules.")
     my_teams_message.send()
 
-def check_yara_rules():
-    for (dirpath, dirname, filenames) in os.walk('C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\extracted_packages'):
+def load_yara_rules():
+    yara_rules_dict = {}
+    for (dirpath, dirname, filenames) in os.walk(current_working_directory + dir_separator + 'yara_rules'):
         if filenames:
             for filename in filenames:
-                rules = yara.compile(sources={
-                    'namespace1':'rule dummy { condition: true }',
-                    'namespace2':'rule dummy2 { condition: true }',
-                    'namespace3':'rule dummy3 { condition: true }'
-                    })
-                yara_matches = rules.match(dirpath + '\\' + filename)
+                with open(current_working_directory + dir_separator + 'yara_rules' + dir_separator + filename, 'r') as yars:
+                    yara_rules_dict[filename] = yars.read()
+
+    return yara_rules_dict
+
+def check_yara_rules():
+    yara_rules = load_yara_rules() # load YARA rules for compiling
+
+    print("\n\nLoaded YARA rules:")
+    for y, r in yara_rules.items():
+        print(y)
+
+    for (dirpath, dirname, filenames) in os.walk(current_working_directory + dir_separator + 'extracted_packages'):
+        if filenames:
+            for filename in filenames:
+                rules = yara.compile(sources=yara_rules)
+                yara_matches = rules.match(dirpath + dir_separator + filename) 
                 if yara_matches:
-                    package_name = dirpath.split('\\')[7]
-                    create_card(package_name, filename, yara_matches)
+                    package_name = dirpath.split(dir_separator)[7]
+                    # create_card(package_name, filename, yara_matches)
                     try:
-                        shutil.move(dirpath + '\\' + filename, 'C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\potentially_malicious\\' + package_name + '\\' + filename)
+                        shutil.move(dirpath + dir_separator + filename, current_working_directory + dir_separator + 'potentially_malicious' + dir_separator + package_name + dir_separator + filename)
                     except:
-                        os.mkdir('C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\potentially_malicious\\' + package_name)
-                        shutil.move(dirpath + '\\' + filename, 'C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\potentially_malicious\\' + package_name + '\\' + filename)
+                        os.mkdir(current_working_directory + dir_separator + 'potentially_malicious' + dir_separator + package_name)
+                        shutil.move(dirpath + dir_separator + filename, current_working_directory + dir_separator + 'potentially_malicious' + dir_separator + package_name + dir_separator + filename)
 
 def clean_up():
-    for (dirpath, dirname, filenames) in os.walk('C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\npm_packages'):
+    for (dirpath, dirname, filenames) in os.walk(current_working_directory + dir_separator + 'npm_packages'):
         for filename in filenames:
-            os.remove(dirpath + '\\' + filename)
-    for (dirpath, dirname, filenames) in os.walk('C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\extracted_packages'):
-        if dirpath != 'C:\\Users\\choff\\Documents\\Python_Scripts\\npm_web_scraper\\extracted_packages':
+            os.remove(dirpath + dir_separator + filename)
+    for (dirpath, dirname, filenames) in os.walk(current_working_directory + dir_separator + 'extracted_packages'):
+        if dirpath != current_working_directory + dir_separator + 'extracted_packages':
             shutil.rmtree(dirpath)
 
+def check_OS(sys_platform):
+    separator = '\\'
+    if sys_platform.startswith('linux') or sys_platform == 'darwin':
+        separator = '/'
+    elif sys_platform == 'win32':
+        separator = '\\'
+    return separator
+
+
 def main():
-    # package_names = get_new_package_names()
-    # download_packages(package_names)
-    # extract_packages()
-    # check_yara_rules()
-    clean_up()
+    package_names = get_new_package_names()
+    download_packages(package_names)
+    extract_packages()
+    check_yara_rules()
+    # clean_up()
 
 if __name__ == '__main__':
+    current_working_directory = os.getcwd()
+    dir_separator = check_OS(sys.platform)
     main()
